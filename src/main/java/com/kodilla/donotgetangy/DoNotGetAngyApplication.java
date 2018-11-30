3e0b6c6d3024a685562bdd6aeee2e9c6ceba6dd7
tymsocha <tymoteusz.socha@outlook.com>
2018-11-30 18:54:44 +0100

    Game of 'Do Not Get Angry'

---
+++ src/main/java/com/kodilla/donotgetangy/DoNotGetAngyApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla.donotgetangy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoNotGetAngyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoNotGetAngyApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kodilla/donotgetangy/DoNotGetAngyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kodilla.donotgetangy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DoNotGetAngyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
