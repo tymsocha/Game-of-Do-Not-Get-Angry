b811d24101f35be574b44bf5dbbcb3c9a28ee57b
tymsocha <tymoteusz.socha@outlook.com>
2018-12-09 17:08:23 +0100

    Cannot add pawn to board

--- src/main/java/com/kodilla/donotgetangy/DoNotGetAngyApplication.java
+++ src/main/java/com/kodilla/donotgetangy/DoNotGetAngyApplication.java
@@ -1,12 +1,34 @@
 package com.kodilla.donotgetangy;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-@SpringBootApplication
-public class DoNotGetAngyApplication {
+
+public class DoNotGetAngyApplication extends Application {
+
+    private Image board = new Image("file:E:/Dokumenty/Kodilla/Kodilla-Projects/do-not-get-angy/src/main/images/board.png");
 
     public static void main(String[] args) {
-        SpringApplication.run(DoNotGetAngyApplication.class, args);
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(board, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, backgroundSize);
+        Background background = new Background(backgroundImage);
+
+        GridPane grid = new GridPane();
+        grid.setBackground(background);
+
+        Scene scene = new Scene(grid, 1200, 900, Color.WHITESMOKE);
+
+        primaryStage.setTitle("Do Not Get Angry");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
